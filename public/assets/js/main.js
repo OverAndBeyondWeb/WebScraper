cda1ce852001477afd031a18dfdf59d27b226c0f
Juan Fox <foxj0875@gmail.com>
2018-04-20 12:26:27 -0400

    scraped articles styled

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,10 +2,15 @@ $('.scrape').click(function() {
   $.post('/scrape', function(data) {
 
     data.forEach(function(article) {
-      var articleEl = $('<div>');
-      articleEl.append('<div class="article-title">' + article.headline + '</div>');
-      articleEl.append('<div class="article-summary">' + article.summary + '</div>');
-      articleEl.append('<div class="article-url">' + article.url + '</div>');
+      var articleEl = $('<div class="article">');
+
+      var articleContent = $('<div class="article-content">')
+      articleContent.append('<div class="article-title">' + article.headline + '</div>');
+      articleContent.append('<div class="article-summary">' + article.summary + '</div>');
+      articleContent.append('<div class="article-url">' + article.url + '</div>');
+
+      articleEl.append(articleContent);
+      articleEl.append('<button class="save-btn">Save Article</button>');
 
       $('.articles').append(articleEl);
     });
